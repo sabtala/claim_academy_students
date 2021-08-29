649eecd718d109e1b94b84c8aa1f83e6adfa0206
sabtala <sabtala05@gmail.com>
2021-08-28 19:39:51 -0500

    created router for list of students

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ListStudentComponent from './Components/ListStudentComponent';
 import HeadedComponent from './Components/HeadedComponent';
 import FooterComponent from './Components/FooterComponent';
@@ -7,11 +8,19 @@ import FooterComponent from './Components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeadedComponent />
-        <div className="container">
-          <ListStudentComponent />
-        </div>
-      <FooterComponent />
+      <Router>
+       
+              <HeadedComponent />
+                <div className="container">
+                  <Switch>
+                          <Route path = "/" component = {ListStudentComponent}></Route>
+                          <Route path = "/students" component = {ListStudentComponent}></Route>
+                         
+                  </Switch>
+                </div>
+              <FooterComponent />
+        
+      </Router>
     </div>
     
   );
